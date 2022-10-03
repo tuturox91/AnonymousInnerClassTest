72cc81498d9e3da24b4cb0d23981590ed7ca8451
John Doe <tuturox91@gmail.com>
2022-10-03 13:02:25 +0300

    Anonymous inner class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        TalkingClock myClock = new TalkingClock();
+
+        myClock.start(1000, true);
+
+        JOptionPane.showMessageDialog(null, "Quit Program?");
+        System.exit(0);
     }
 }
